1f9ea89cff0003392ef81b7ecb8e9c680825e6bf
Almedina Heric <almedina.heric@edu.fit.ba>
2022-11-25 22:23:38 +0100

    prviCommit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ERezervacijaAngular
+# eRezervacijaAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+    RouterModule.forRoot(
+      [
+        //ovdje idu rute
+
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
